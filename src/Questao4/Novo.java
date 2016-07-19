11b3ee9ce5a58c5a2e8b771d81ee4bf56db4848f
FranklinBasilio <basilius@ymail.com>
2016-07-19 20:04:19 -0300

    exercicio valendo.. 19 d ejulho, atualizar

---
+++ src/Questao4/Novo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Questao4;
+
+/**
+ *
+ * @author usuario
+ */
+public class Novo extends Imovel{
+    //adcional de preco
+    
+    //acesso aos metodos e impressao deste valor adcional
+}
---
+++ src/Questao4/Velho.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Questao4;
+
+/**
+ *
+ * @author usuario
+ */
+public class Velho extends Imovel{
+    //desconto de preço
+    //criar metodos de acesso e impressao
+}
